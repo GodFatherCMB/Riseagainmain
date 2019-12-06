fff3926a9ba8710ebd73c012e6c88917da5c1f7b
GodFatherCMB <43375989+GodFatherCMB@users.noreply.github.com>
2019-12-06 10:22:00 +0000

    Main game
    Game plus exe

---
+++ Rise again/Assets/Platformer Template/Scripts/Interaction/DeathZone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Platformer
+{
+    public class DeathZone : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.transform.tag == "Player")
+            {
+                //if player enter to this trigger = gameover
+                collision.gameObject.GetComponent<PlayerController>().Death();
+                GetComponent<AudioSource>().Play();
+            }
+        }
+
+    }
+}
